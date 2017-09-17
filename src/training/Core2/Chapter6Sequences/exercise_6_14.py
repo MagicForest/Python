c68d5bf6ec5369112b67364e3d7757de1af60ff4
Grass.Winter <quietwolf@yeah.net>
2017-09-17 22:53:44 +0800

    add exercise-6-14

---
+++ src/training/Core2/Chapter6Sequences/exercise_6_14.py
@@ -0,0 +1,13 @@
+
+def rock_paper_scissors():
+
+    return result
+
+
+def test_rock_paper_scissors():
+
+    print '------test_rock_paper_scissors is passed. ^__^-----'
+
+
+if __name__ == '__main__':
+    test()
