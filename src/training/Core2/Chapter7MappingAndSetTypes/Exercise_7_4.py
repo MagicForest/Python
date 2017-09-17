636d2d55c5dae51d04478fef1b2d145e9ebbd113
Grass.Winter <quietwolf@yeah.net>
2017-09-17 23:47:11 +0800

    add Exercise7-4 build dict with two list.

---
+++ src/training/Core2/Chapter7MappingAndSetTypes/Exercise_7_4.py
@@ -0,0 +1,2 @@
+def build_dict_with_two_list(list1, list2):
+    return dict(zip(list1, list2))
---
+++ src/training/Core2/Chapter7MappingAndSetTypes/TestExercise_7_4.py
@@ -0,0 +1,13 @@
+import  Exercise_7_4
+
+def test_build_dict_with_two_list():
+    assert {1:'abc', 2:'def', 3:'ghi'} ==  Exercise_7_4.build_dict_with_two_list( [i for i in range(1, 4)],
+                                                                                  ['abcdefghi'[i: i+3] for i in range(0, 9, 3)])
+
+
+def test():
+    test_build_dict_with_two_list()
+
+
+if __name__ == '__main__':
+    test()
