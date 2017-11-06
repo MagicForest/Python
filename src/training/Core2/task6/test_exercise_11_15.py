942501bfb947048be22d25aeee582a1dcfcf9c1e
Grass.Winter <quietwolf@yeah.net>
2017-11-07 07:51:48 +0800

    update test11_15

--- src/training/Core2/task6/test_exercise_11_15.py
+++ src/training/Core2/task6/test_exercise_11_15.py
@@ -1,14 +1,11 @@
-import exercise_11_14
+import exercise_11_15
 
 
-def test_fibonacci():
-    assert 1 == exercise_11_14.fibonacci(1)
-    assert 1 == exercise_11_14.fibonacci(2)
-    assert 2 == exercise_11_14.fibonacci(3)
-    assert 8 == exercise_11_14.fibonacci(6)
+def test():
+   assert True
 
 
 
 
 if __name__ == "__main__":
-    test_fibonacci()  
+    test()  
