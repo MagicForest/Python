6bb015336a351e94b155360eddc3acd458fd35d0
Grass.Winter <quietwolf@yeah.net>
2017-12-19 06:50:30 +0800

    add 15-21

---
+++ src/training/Core2/Chapter15RegularExpressions/exercise15_21.py
@@ -0,0 +1,10 @@
+import re
+
+
+def get_month(a_string):
+    return re.findall('\d{4}-(\d{2})-\d{2}\s\d{2}:\d{2}:\d{2}', a_string)
+
+if __name__ == '__main__':
+    assert ['12'] == get_month('abc2017-12-19 10:10:10x')
+    assert ['12', '01'] == get_month('abc2017-12-19 10:10:10x2016-01-11 11:11:11')
+    print 'all passed.'
