8af56e9384061504f05b229467c922ba71a433cb
Grass.Winter <quietwolf@yeah.net>
2017-12-19 07:47:59 +0800

    modify 15-21

--- src/training/Core2/Chapter15RegularExpressions/exercise15_21.py
+++ src/training/Core2/Chapter15RegularExpressions/exercise15_21.py
@@ -2,7 +2,7 @@ import re
 
 
 def get_month(a_string):
-    return re.findall('\d{4}-(\d{2})-\d{2}\s\d{2}:\d{2}:\d{2}', a_string)
+    return re.findall('\d{4}-(\d{2})-\d{2}\s\d{2}(?::\d{2}){2}', a_string)
 
 if __name__ == '__main__':
     assert ['12'] == get_month('abc2017-12-19 10:10:10x')
