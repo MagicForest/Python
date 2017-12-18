5007466f9766ad2c481dba16bea1266ad2d6666a
Grass.Winter <quietwolf@yeah.net>
2017-12-19 05:14:41 +0800

    exercise 15-8

---
+++ src/training/Core2/Chapter15RegularExpressions/exercise15_8.py
@@ -0,0 +1,15 @@
+import re
+
+
+def match_int(a_string):
+    result = re.search('-?\d+[Ll]', a_string)
+    if result is not None:
+        return result.group()
+
+if __name__ == '__main__':
+    assert '1L' == match_int('a1Lb')
+    assert '12l' == match_int('12la')
+    assert '-1l' == match_int('b-1l')
+    assert '-1000L' == match_int('d-1000L')
+    assert match_int('d-1000') is None
+    print 'all passed.'
